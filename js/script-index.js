009c35a5ee478aa61bbc75181a214e2206683350
Camila Sáez <cfsaezbravo@gmail.com>
2017-07-10 14:04:55 -0400

    Intentnado el paso 6

--- js/script-index.js
+++ js/script-index.js
@@ -17,48 +17,28 @@ $(document).ready(function(){
 
 	renderHighlightedRecipes(recipesArray);
 
-	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
-	/*renderHighlightedRecipes(recipesArray);*/
+	renderActivities(activitiesArray);
 
 });
 
-
-/*
-* Función que se encarga de pintar TODAS las recetas que tengan 
-* marcado el atributo "highlighted" como TRUE
-*/
-
-
-/*
-* Función que se encarga de pintar UNA recetas que tenga 
-* marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-recipe.html"
-*/
 function renderRecipe(recipe) {
 
 	$('.list-recipes').append('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">' + recipe.title + '</span><span class="metadata-recipe"><span class="author-recipe">' + recipe.source.name + '</span><span class="bookmarks-recipe"><span class="icon-bookmark"></span> </span></span></span><img src="img/recipes/320x350/' + recipe.name + '.jpg"/></a>');
-
-	console.log('Voy a pintar la receta: ', recipe);
 	
 }
 
-
-
-/*
-* Función que se encarga de pintar todas las actividades
-*/
 function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+
+	activitiesArray.forEach(function renderActivity(recipe){
+		if(activitiesArray.length > 0){
+			$('.wrapper-message').hide();
+		}
+	});
+
+	$('.list-activities').append('<a href="#" class="item-activity"><span class="attribution"><span class="avatar"><img src="img/acitivity/' + activitiesArray.userAvatar + 'class="image-avatar"></span><span class="meta"><span class="author">' + acitivitiesArray.userName + '</span> made <span class="recipe">' + acitivitiesArray.recipeName + '</span>:' + activitiesArray.text + '<span class="location">&mdash;' + activitiesArray.place + '</span></span></span><div class="bg-image" style="background-image: url(' + activitiesArray.image + ');"></div></a>')
 }
 
-/*
-* Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
-* archivo "templates/templates-activity.html"
-*/
-function renderActivity(recipe) {
-	
-}
+
+
 
 
