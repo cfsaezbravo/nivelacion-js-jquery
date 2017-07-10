e390ce76a87f9160d8b9700c8c660e9ea2aa2f15
Camila Sáez <cfsaezbravo@gmail.com>
2017-07-10 13:20:22 -0400

    Terminé la función donde se llamaba la otra función que llama las imagenes y los títulos

--- js/script-index.js
+++ js/script-index.js
@@ -1,10 +1,21 @@
-$(document).ready( function(){
+$(document).ready(function(){
 
 	function printNews(){
 		$('.news').append('<p class="apendice">NUEVAS NOTICIAS</p>');
 	}
 
 	printNews();
+
+	function renderHighlightedRecipes(recipesArray){
+
+	recipesArray.forEach(function(e){
+		if(e.highlighted == true){
+			renderRecipe(e);
+			};
+		});
+	};
+
+	renderHighlightedRecipes(recipesArray);
 
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	/*renderHighlightedRecipes(recipesArray);*/
@@ -16,8 +27,7 @@ $(document).ready( function(){
 * Función que se encarga de pintar TODAS las recetas que tengan 
 * marcado el atributo "highlighted" como TRUE
 */
-/*function renderHighlightedRecipes(recipesArray) {
-	}
+
 
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
@@ -26,7 +36,10 @@ $(document).ready( function(){
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
+
+
 	console.log('Voy a pintar la receta: ', recipe);
+	
 }
 
 
